0aa4c4d84c07e3afbee6ec0212f7855f71f202bd
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-05-11 19:10:34 +0300

    added theme

--- src/js/templating.js
+++ src/js/templating.js
@@ -1,7 +1,8 @@
-import menuItemsTemplate from '../templates/menu-items.hbs';
+import makeMarkupTpl from '../templates/menu-items.hbs';
 import menu from '../menu.json';
 
 const menuRef = document.querySelector('.js-menu');
 
-const markup = menuItemsTemplate(menu);
+const markup = makeMarkupTpl(menu);
 menuRef.insertAdjacentHTML('beforeend', markup);
+// menuRef.innerHTML = markup; // - ⬆️ the same record⬆️
--- src/js/theme.js
+++ src/js/theme.js
@@ -2,3 +2,44 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+// ************ 1 вариант ************ //
+
+const checkbox = document.querySelector('.theme-switch__toggle');
+
+checkbox.addEventListener('click', changeTheme);
+
+function changeTheme() {
+  if (document.body.classList.contains(Theme.LIGHT)) {
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+  localStorage.setItem('theme', Theme.LIGHT);
+}
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === Theme.DARK) {
+  document.body.classList.add(Theme.DARK);
+  checkbox.checked = true;
+} else document.body.classList.add(Theme.LIGHT);
+
+// ************ 2 вариант ************ //
+// const checkbox = document.querySelector('.theme-switch__toggle');
+// checkbox.addEventListener('change', changeTheme);
+
+// function changeTheme(event) {
+//   if (event.currentTarget.checked) {
+//     document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+//     localStorage.setItem('theme', JSON.stringify(Theme.DARK));
+//   } else {
+//     document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+//     localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
+//   }
+// }
+
+// const currentTheme = localStorage.getItem('theme');
+// if (currentTheme === Theme.DARK) {
+//   const parsedTheme = JSON.parse(currentTheme);
+//   document.body.classList.add(parsedTheme);
+//   checkbox.checked = true;
+// } else document.body.classList.add(Theme.LIGHT);
